07b111a6a42e1a8f9f744fd2e2d1b1bc0babda84
turizsolt <turi.zsolt.m@gmail.com>
2021-01-11 20:51:08 +0100

    chore: inited workspace for simple js sniplets

---
+++ index.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const canvas = document.getElementById(
+        'render-canvas'
+    );
+    const renderEngine = new BABYLON.Engine(canvas, true);
+    const createScene = () => {
+        const scene = new BABYLON.Scene(renderEngine);
+
+        const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0));
+        camera.attachControl(canvas, true);
+        const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0));
+        const box = BABYLON.MeshBuilder.CreateBox("box", {});
+        return scene;
+    }
+
+    const scene = createScene();
+
+    renderEngine.runRenderLoop(() => {
+        scene.render();
+    });
+});
