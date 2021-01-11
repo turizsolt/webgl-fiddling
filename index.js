e685d0967f8c5afa3d3cbdbc0a0a7db05b9660da
turizsolt <turi.zsolt.m@gmail.com>
2021-01-11 22:20:51 +0100

    feat: n spinning objects

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const count = 100;
+    const count = 2000;
     const objects = [];
     for (let i = 0; i < count; i++) {
         objects.push({
-            position: { x: 0, y: 0, z: 0 },
-            radius: 1,
-            rotation: 0,
-            speed: 1
+            position: { x: Math.random() * 100 - 50, y: Math.random() * 100 - 50, z: 0 },
+            radius: 5 + Math.random() * 20,
+            rotation: Math.random() * Math.PI * 2,
+            speed: Math.random() * 2
         });
     }
     let framesElapsed = 0;
@@ -20,24 +20,32 @@ window.addEventListener('DOMContentLoaded', () => {
         const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0));
         camera.attachControl(canvas, true);
         const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0));
-        const box = BABYLON.MeshBuilder.CreateBox("box", {});
-        return { scene, box };
+        const boxes = [];
+        for (let i = 0; i < count; i++) {
+            boxes.push(BABYLON.MeshBuilder.CreateBox("box", {}));
+        }
+        return { scene, boxes };
     }
 
-    const { scene, box } = createScene();
+    const { scene, boxes } = createScene();
 
     renderEngine.runRenderLoop(() => {
         scene.render();
-        fpsLabel.innerHTML = renderEngine.getFps().toFixed() + " fps";
 
         framesElapsed++;
-        const rot = objects[0].rotation + objects[0].speed * framesElapsed / Math.PI / 10;
-        box.position.x = objects[0].position.x + objects[0].radius * Math.sin(rot);
-        box.position.y = objects[0].position.y - objects[0].radius * Math.cos(rot);
-        box.position.z = objects[0].position.z;
+        if (framesElapsed % 100 === 0) {
+            fpsLabel.innerHTML = renderEngine.getFps().toFixed() + " fps";
+        }
+
+        for (let i = 0; i < count; i++) {
+            const rot = objects[i].rotation + objects[i].speed * framesElapsed / Math.PI / 10;
+            boxes[i].position.x = objects[i].position.x + objects[i].radius * Math.sin(rot);
+            boxes[i].position.y = objects[i].position.y - objects[i].radius * Math.cos(rot);
+            boxes[i].position.z = objects[i].position.z;
+        }
     });
 
     window.addEventListener("resize", function () {
-        engine.resize();
+        renderEngine.resize();
     });
 });
